90236f9d15c2fed3214ebda3422d656c9d6dad55
vinnarasu <vinnarasu2000@gmail.com>
2025-01-08 22:25:12 +0530

    Book store
    CRUD OPERATIONS

---
+++ src/main/java/com/vinna/bookstore/BookstoreApplication.java
@@ -0,0 +1,13 @@
+package com.vinna.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreApplication.class, args);
+	}
+
+}
